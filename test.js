0e741466c657a4a46653ced91c140970ccc56fbc
ravi <ravikr845430@gmail.com>
2025-12-25 12:25:00 +0530

    ll

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ console.log("Hello World, This is Ravi.")
 let sum = 1;
 for(let i=0;i<10;i++){
     sum +=i;
-    console.log("Sum is: ",sum);
+    console.log("Sum is: ",sum;
     console.log("Multiplication is: ",sum * i)
 }
