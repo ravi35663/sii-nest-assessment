c53c468157edcb79fa4950429af2c819c0c9aae6
ravi <ravikr845430@gmail.com>
2025-12-25 12:09:42 +0530

    learning jenkins

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 console.log("Hello World, This is Ravi.")
-let sum = 0;
+let sum = 1;
 for(let i=0;i<10;i++){
     sum +=i;
     console.log("Sum is: ",sum);
+    console.log("Multiplication is: ",sum * i)
 }
